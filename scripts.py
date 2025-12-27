d4006ca0cfa62e24b157dbe03ec41e0e35f0aca1
Pavelwell7 <pavelbugaev303@gmail.com>
2025-12-27 23:46:06 +0300

    Update scripts.py

--- scripts.py
+++ scripts.py
@@ -2,61 +2,62 @@ import random
 
 from datacenter.models import Schoolkid, Commendation, Mark, Chastisement, Lesson
 
-
-def fix_marks(schoolkid):
+def get_schoolkid_name(schoolkid):
 
     try:
-        kid = Schoolkid.objects.get(full_name__contains=schoolkid)
-        bad_marks = Mark.objects.filter(schoolkid=kid, points__in=[2, 3])
-        updated_count = bad_marks.update(points=5)
-        return updated_count
+        return Schoolkid.objects.get(full_name__contains=schoolkid)
     except Schoolkid.DoesNotExist:
         print(f"Ученик '{schoolkid}' не найден!!!")
         return None
     except Schoolkid.MultipleObjectsReturned:
         print(f"Найдено несколько учеников с именем '{schoolkid}'. Напишите полностью ФИО!!!")
         return None
+
+
+def fix_marks(schoolkid):
+
+    kid = get_schoolkid_name(schoolkid)
+    if not kid:
+        return None
+    bad_marks = Mark.objects.filter(schoolkid=kid, points__in=[2, 3])
+    updated_count = bad_marks.update(points=5)
+    return updated_count
 
 
 def remove_chastisements(schoolkid):
 
-    try:
-        kid = Schoolkid.objects.get(full_name__contains=schoolkid)
-        chastisements = Chastisement.objects.filter(schoolkid=kid)
-        return chastisements.delete()
-    except Schoolkid.DoesNotExist:
-        print(f"Ученик '{schoolkid}' не найден!!!")
+    kid = get_schoolkid_name(schoolkid)
+    if not kid:
         return None
-    except Schoolkid.MultipleObjectsReturned:
-        print(f"Найдено несколько учеников с именем '{schoolkid}'. Напишите полностью ФИО!!!")
-        return None
+    chastisements = Chastisement.objects.filter(schoolkid=kid)
+    return chastisements.delete()
 
 
-def create_commendation(schoolkid):
-
-    try:
-        commendation_list = ["Молодец!",
-                             "Отлично",
-                             "Хорошо",
-                             "Гораздо лучше, чем я ожидал!",
-                             "Ты меня приятно удивил!",
-                             "Великолепно!",
-                             "Прекрасно!Ты меня очень обрадовал!",
-                             "Именно этого я давно ждал от тебя!",
-                             "Сказано здорово – просто и ясно!",
-                             ]
-        kid = Schoolkid.objects.get(full_name__contains = schoolkid)
-        lesson_math = Lesson.objects.filter(year_of_study=6, group_letter='А', subject__title="Музыка")
-        last_math_lesson = lesson_math.order_by("-date").first()
-        commendation = Commendation.objects.create(text=random.choice(commendation_list),
-                                               created=last_math_lesson.date, schoolkid=kid,
-                                               subject=last_math_lesson.subject, teacher=last_math_lesson.teacher)
-        return commendation
-    except Schoolkid.DoesNotExist:
-        print(f"Ученик '{schoolkid}' не найден!!!")
+def create_commendation(schoolkid, year_of_study, group_letter, subject):
+    commendation_list = [
+        "Молодец!",
+        "Отлично",
+        "Хорошо",
+        "Гораздо лучше, чем я ожидал!",
+        "Ты меня приятно удивил!",
+        "Великолепно!",
+        "Прекрасно!Ты меня очень обрадовал!",
+        "Именно этого я давно ждал от тебя!",
+        "Сказано здорово – просто и ясно!",
+    ]
+    kid = get_schoolkid_name(schoolkid)
+    if not kid:
         return None
-    except Schoolkid.MultipleObjectsReturned:
-        print(f"Найдено несколько учеников с именем '{schoolkid}'. Напишите полностью ФИО!!!")
+    lessons = Lesson.objects.filter(year_of_study=year_of_study, group_letter=group_letter, subject__title=subject)
+    last_lesson = lessons.order_by("-date").first()
+    if not last_lesson:
+        print(f"Урок '{subject}' для {year_of_study}{group_letter} не найден!")
         return None
+    commendation = Commendation.objects.create(
+                                                text=random.choice(commendation_list),
+                                                created=last_lesson.date, schoolkid=kid,
+                                                subject=last_lesson.subject, teacher=last_lesson.teacher
+                                                )
+    return commendation
 
 
